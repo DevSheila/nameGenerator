f0096ce7bd6a9e395590e9fc391f2b53c35af2d5
sheila <devsheilawambui@gmail.com>
2021-02-19 17:42:40 +0300

    decarling global variables

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// #itemForm 
+// #genderSelector
+// #bdate
+// #btnSubmit
+
+const form = document.getElementById("itemForm")
+const gender = document.getElementById("genderSelector");
+const bDate = document.getElementById("bDate");
+const btnSubmit = document.getElementById("btnSubmit");
