18ab672e875a930c09635808d94b4086b9d3db6d
sheila <devsheilawambui@gmail.com>
2021-02-19 17:48:48 +0300

    Created funnction to assign name

--- js/main.js
+++ js/main.js
@@ -26,3 +26,52 @@ function validateForm(){
         console.log("All form fields have been filled")
     }
 }
+
+//----------------------------------------Determining Name-----------------------------
+
+function getName(){
+    //determining day born
+        var daysOfWeek =['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+        var d = new Date(bDate.value);
+        var dayBorn = daysOfWeek[d.getDay()];
+        var name;
+    //determining gender name
+    if(gender.value == "male"){
+        if(dayBorn == "Sunday"){
+            name="Kwasi";
+        }else if(dayBorn == "Monday"){
+            name="Kwadwo";
+        }else if(dayBorn == "Tuesday"){
+            name="Kwabena";
+        }else if(dayBorn == "Wednesday"){
+            name="  Kwaku";
+        }else if(dayBorn == "Thursday"){
+            name="Yaw";
+        }else if(dayBorn == "Friday"){
+            name="Kofi";
+        }else{
+            name="Kwame";
+        }
+        
+        alert("Your Gender: "+gender.value + " Day Born: "+ dayBorn +" Ghanian Name: " +name );
+    }else {
+        if(dayBorn == "Sunday"){
+            name="Akosau";
+        }else if(dayBorn == "Monday"){
+            name="Adwao";
+        }else if(dayBorn == "Tuesday"){
+            name="Abenna";
+        }else if(dayBorn == "Wednesday"){
+            name="Akkua";
+        }else if(dayBorn == "Thursday"){
+            name="Yaa";
+        }else if(dayBorn == "Friday"){
+            name="Afua";
+        }else{
+            name="Ama";
+        }
+        alert("Your Gender: "+gender.value + " Day Born: "+ dayBorn +" Ghanian Name: " +name );  
+    }
+
+    
+}
