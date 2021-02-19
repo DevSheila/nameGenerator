1e3af6a6fdf16c48d7ce19deb922c7d066a6f937
sheila <devsheilawambui@gmail.com>
2021-02-19 17:43:28 +0300

    adding event listener to form

--- js/main.js
+++ js/main.js
@@ -7,3 +7,10 @@ const form = document.getElementById("itemForm")
 const gender = document.getElementById("genderSelector");
 const bDate = document.getElementById("bDate");
 const btnSubmit = document.getElementById("btnSubmit");
+
+form.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    console.log(gender.value + ""+bDate.value );
+    validateForm();
+    
+});
